f7b5f6e351321fd2885d70dd171974bc38d8a551
Fsaadatian <fa.saadatian@gmail.com>
2024-11-02 21:36:44 +0330

    Add files via upload

---
+++ Matrices.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatricesSaadatian
+{
+    internal class Matrices
+    {
+        public static void SumMatris(int rowMatris1, int columnMatris1, int rowMatris2, int columnMatris2, int[,] matris1, int[,] matris2)
+        {
+            if (rowMatris1 != rowMatris2 || columnMatris1 != columnMatris2)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Sorry, these two matrices are not the same size to sum.");
+                Console.ResetColor();
+            }
+            else
+            {
+                int[,] sumMatris = new int[rowMatris1,columnMatris1];
+                for (int i = 0; i < rowMatris1; i++)
+                {
+                    for (int j = 0; j < columnMatris1; j++)
+                    {
+                        sumMatris[i,j] = matris1[i,j] + matris2[i, j];
+
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Sum of the two matrices is:");
+                ShowResult(sumMatris, rowMatris1, columnMatris1);
+            }
+        }
+        public static void MultiMatris(int rowMatris1, int columnMatris1, int rowMatris2, int columnMatris2, int[,] matris1, int[,] matris2)
+        {
+            if (columnMatris1 != rowMatris2)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Sorry, these two matrices have not the suitable size to multiply.");
+                Console.ResetColor();
+            }
+            else
+            {
+                int[,] multiMatris = new int[rowMatris1, columnMatris2];
+                for (int i = 0; i < rowMatris1; i++)
+                {
+                    for (int j = 0; j < columnMatris2; j++)
+                    {
+                        for (int k = 0; k < columnMatris1; k++)
+                        {
+                            multiMatris[i, j] += matris1[i, k] * matris2[k, j];
+                        }
+                        
+
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"Multiplication of the two matrices is a matris{rowMatris1}*{columnMatris2} :");
+                ShowResult(multiMatris, rowMatris1, columnMatris2);
+                
+            }
+        }
+        private static void ShowResult(int[,] matris, int rowMatris, int columnMatris)
+        {
+            Console.ResetColor();
+            for (int i = 0; i < rowMatris; i++)
+            {
+                for (int j = 0; j < columnMatris; j++)
+                {
+                    Console.Write($"{matris[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MatricesSaadatian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count = 0;
+            Console.WriteLine("Please, enter number of rows for the matris No. 1:");
+            int rowMatris1 = int.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine("Please, enter number of columns for the matris No. 1:");
+            int columnMatris1 = int.Parse(Console.ReadLine() ?? "");
+            int [ , ] matris1 = new int[rowMatris1, columnMatris1];
+            Console.WriteLine("Please, enter elements of the matris No. 1:");
+            for (int i = 0; i < rowMatris1; i++)
+            {
+                for (int j = 0; j < columnMatris1; j++)
+                {
+                        try
+                        {
+                            matris1[i, j] = int.Parse(Console.ReadLine() ?? "");
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("Sorry, element is invalid. Try again!");
+                            Console.ResetColor();
+                            j = j - 1;
+                        }
+                }
+            }
+            
+            Console.WriteLine("Please, enter number of rows for the matris No. 2:");
+            int rowMatris2 = int.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine("Please, enter number of columns for the matris No. 2:");
+            int columnMatris2 = int.Parse(Console.ReadLine() ?? "");
+            int[,] matris2 = new int[rowMatris2, columnMatris2];
+            Console.WriteLine("Please, enter elements of the matris No. 2:");
+            for (int i = 0; i < rowMatris2; i++)
+            {
+                for (int j = 0; j < columnMatris2; j++)
+                {
+                    try
+                    {
+                        matris2[i, j] = int.Parse(Console.ReadLine() ?? "");
+                        
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Sorry, element is invalid. Try again!");
+                        Console.ResetColor();
+                        j = j - 1;
+
+                    }
+                }
+            }
+            // Two matrix summation
+            Matrices.SumMatris(rowMatris1 , columnMatris1, rowMatris2, columnMatris2 , matris1, matris2);
+
+            //Two matrix multiplication
+            Matrices.MultiMatris(rowMatris1, columnMatris1, rowMatris2, columnMatris2, matris1, matris2);
+
+
+        }
+    }
+}
